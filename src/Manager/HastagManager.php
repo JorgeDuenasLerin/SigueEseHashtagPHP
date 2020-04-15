f3a91c5e702268c61fe41741935ba7da2712b07a
JorgeDuenasLerin <jorge.duenas.lerin@gmail.com>
2020-04-15 18:51:55 +0200

    TODO. Retoques en el primer manager

--- src/Manager/HastagManager.php
+++ src/Manager/HastagManager.php
@@ -1,7 +1,7 @@
 <?php
 
-
-  class hastagManager implements IDWESEntidadManager{
+// TODO: Cambiar nombre de ficher por nombre bueno
+  class HashtagManager implements IDWESEntidadManager{
 
     public static function getAll(){
       $db = DWESBaseDatos::obtenerInstancia();
@@ -11,22 +11,28 @@
 
     public static function getById($id){
       $db = DWESBaseDatos::obtenerInstancia();
+      // TODO: Consulta preparada
+      // "SELECT * FROM HASTAG WHERE ID = ?"
       $db->ejecuta("SELECT * FROM HASTAG "." WHERE ID = '$id'");
       return $db->obtenDatos()[0];
     };
 
     public static function insert(...$campos){
       $db = DWESBaseDatos::obtenerInstancia();
+      //TODO: Cuidado con el campo autoincremental
 			$db->ejecuta('INSERT INTO HASTAG (ID,NOMBRE) VALUES(?,?)',$campos);
     }
 
     public static function update($id, ...$campos){
       $db = DWESBaseDatos::obtenerInstancia();
+      //TODO: Actualizar nombre XXXX para id N
+      //TODO: Preparar consultas
 			$db->ejecuta("UPDATE HASTAG SET ID='$id' WHERE ID ='$id';");
     }
 
     public static function delete($id){
 			$db = DWESBaseDatos::obtenerInstancia();
+      //TODO: Preparar consulta
 			$db->ejecuta("DELETE FROM HASTAG WHERE ID='$id'");
 		}
 

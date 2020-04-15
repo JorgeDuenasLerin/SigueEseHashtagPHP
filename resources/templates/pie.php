d5e117b21daa9cb78c895e990839abd366ede711
adriansanz94 <adriansanzperez.94@gmail.com>
2020-04-15 19:09:31 +0200

    usuarioManager

--- resources/templates/pie.php
+++ resources/templates/pie.php
@@ -1,6 +1,10 @@
 <br>
-<div class="">
-  <h4>Pie en fichero separado</h4>
-  Div de mi empresa.
-  Localización, etc.
-</div>
+
+<link rel="stylesheet" href="/css/footer.css">
+<footer>
+  <div class="">
+    <h4>Pie en fichero separado</h4>
+    Div de mi empresa.
+    Localización, etc.
+  </div>
+</footer>
--- src/Manager/UsuarioManager.php
+++ src/Manager/UsuarioManager.php
@@ -3,11 +3,52 @@
 
   class usuarioManager implements IDWESEntidadManager{
 
-    public static function getAll();
-    public static function getById($id);
-    public static function insert(...$campos);
-    public static function update($id, ...$campos);
-    public static function delete($id);
+    public static function autentificando($nombre){
+      $db = DWESBaseDatos::obtenerInstancia();
+      $db->ejecuta("SELECT id,nombre,pass,email 
+                    FROM  USUARIO 
+                    WHERE NOMBRE = ? ",
+                    $nombre);
+      return $db->obtenDatos();
+    }
+    public static function getAll(){
+      $db = DWESBaseDatos::obtenerInstancia();
+      $db->ejecuta("SELECT * FROM  USUARIO");
+      return $db->obtenDatos();
+    }
+    public static function getById($id){
+      $db = DWESBaseDatos::obtenerInstancia();
+      $db->ejecuta("SELECT * FROM USUARIO WHERE ID = ?", $id);
+      $fila = $db->obtenDatos()[0];
+      print_r($fila);
+      return
+      new Usuario(
+        $fila['id'],
+        $fila['nombre'],
+        $fila['pass'],
+        $fila['email']
+      );
+    }
+    public static function insert(...$campos){
+      $db = DWESBaseDatos::obtenerInstancia();
+      $db->ejecuta("INSERT INTO USUARIO (nombre, pass, email)
+                    VALUES (?, ?, ?)",
+                    $campos);
+      return $db->getLastId();
+    }
+    public static function update($id, ...$campos){
+      $db = DWESBaseDatos::obtenerInstancia();
+       $db->ejecuta("UPDATE USUARIO
+                    SET NOMbRE = ?,
+                        PASS = ?,
+                        EMAIL = ?
+                    WHERE ID = ?",
+                    $campos);
+    }
+    public static function delete($id){
+      $db = DWESBaseDatos::obtenerInstancia();
+      $db->ejecuta("DELETE FROM USUARIO WHERE ID = ?", $id);
+    }
   }
 
 ?>

f275b8458c4c90a3e1b942d66037dd4ef46a0d15
adriansanz94 <adriansanzperez.94@gmail.com>
2020-04-16 10:58:36 +0200

    clases manager creadas

--- src/Manager/HashtagManager.php
+++ src/Manager/HashtagManager.php
@@ -13,27 +13,29 @@
       $db = DWESBaseDatos::obtenerInstancia();
       // TODO: Consulta preparada
       // "SELECT * FROM HASTAG WHERE ID = ?"
-      $db->ejecuta("SELECT * FROM HASHTAG  WHERE ID = ?");
+      $db->ejecuta("SELECT * FROM HASHTAG  WHERE ID = ?",$id);
       return $db->obtenDatos()[0];
     }
 
     public static function insert(...$campos){
       $db = DWESBaseDatos::obtenerInstancia();
       //TODO: Cuidado con el campo autoincremental
-			$db->ejecuta('INSERT INTO HASHTAG (ID,NOMBRE) VALUES(?,?)',$campos);
+			$db->ejecuta("INSERT INTO HASHTAG (ID,NOMBRE,ID_USUARIO) 
+                    VALUES(?,?,?)",
+                    $campos);
     }
 
     public static function update($id, ...$campos){
       $db = DWESBaseDatos::obtenerInstancia();
       //TODO: Actualizar nombre XXXX para id N
       //TODO: Preparar consultas
-			$db->ejecuta("UPDATE HASHTAG SET ID='$id' WHERE ID ='$id';");
+			$db->ejecuta("UPDATE HASHTAG SET ID = ? WHERE ID = ?",$campos);
     }
 
     public static function delete($id){
 			$db = DWESBaseDatos::obtenerInstancia();
       //TODO: Preparar consulta
-			$db->ejecuta("DELETE FROM HASHTAG WHERE ID='$id'");
+			$db->ejecuta("DELETE FROM HASHTAG WHERE ID = ?",$id);
 		}
 
 ?>
--- src/Manager/PublicacionManager.php
+++ src/Manager/PublicacionManager.php
@@ -1,24 +1,21 @@
 <?php 
 
 class PublicacionManager implements IDWESEntidadManager{
+
   public static function getAll(){
     $db = DWESBaseDatos::obtenerInstancia();
-    $db->ejecuta('SELECT ID,NOMBRE,CONTENIDO,IMAGEN,FECHA,APLICACION 
-                  FROM PUBLICACION');
+    $db->ejecuta('SELECT * FROM PUBLICACION');
     return $db->obtenDatos();
   }
 
   public static function getById($id){
     $db = DWESBaseDatos::obtenerInstancia();
-    // TODO: Consulta preparada
-    // "SELECT * FROM HASTAG WHERE ID = ?"
-    $db->ejecuta("SELECT * FROM PUBLICACION WHERE ID = ?");
+    $db->ejecuta("SELECT * FROM PUBLICACION WHERE ID = ?",$id);
     return $db->obtenDatos()[0];
   }
 
   public static function insert(...$campos){
     $db = DWESBaseDatos::obtenerInstancia();
-    //TODO: Cuidado con el campo autoincremental
     $db->ejecuta("INSERT INTO PUBLICACION 
                     (ID,NOMBRE,CONTENIDO,IMAGEN,FECHA,APLICACION)
                    VALUES(?,?,?,?,?,?)",
@@ -27,8 +24,6 @@ class PublicacionManager implements IDWESEntidadManager{
 
   public static function update($id, ...$campos){
     $db = DWESBaseDatos::obtenerInstancia();
-    //TODO: Actualizar nombre XXXX para id N
-    //TODO: Preparar consultas
     $db->ejecuta("UPDATE PUBLICACION 
                   SET ID = ?,
                       NOMBRE = ?,
@@ -41,8 +36,7 @@ class PublicacionManager implements IDWESEntidadManager{
 
   public static function delete($id){
     $db = DWESBaseDatos::obtenerInstancia();
-    //TODO: Preparar consulta
-    $db->ejecuta("DELETE FROM PUBLICACION WHERE ID=?");
+    $db->ejecuta("DELETE FROM PUBLICACION WHERE ID=?",$id);
   }
 
 

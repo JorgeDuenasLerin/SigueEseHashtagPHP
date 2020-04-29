9b9b68109ca5987fb0d4c394c1fb50ffd19195cb
adriansanz94 <adriansanzperez.94@gmail.com>
2020-04-29 10:53:26 +0200

    modificado inicio sesion

--- resources/templates/navegacion.php
+++ resources/templates/navegacion.php
@@ -3,8 +3,19 @@ session_start();
 global $ROOT;
 
 //cerrar sesion
+if(isset($_GET['cerrarSsion']) && $_GET['cerrarSesion'] == true){
+  $_SESSION['autentificado'] = false;
+  session_destroy();
+
+  header('location: inico.php');
+  die();
+}
 
 
+//ubtenemos id del usuario
+if(isset($_GET['ID'])){
+  $id = intval($_GET['ID']);
+}
 
 //obtener id del usuario
 
@@ -22,9 +33,14 @@ global $ROOT;
   </div>
   <nav>
     <div class="menu">
-      <a href="inicio.php">Inicio</a>
-      <a href="login.php">login</a>
-      <a href="paginaajax.php">AJAX</a>
+    <a href="inicio.php">Inicio</a>
+      <?php if(isset($_SESSION['autentificado']) && $_SESSION['autentificado'] == true ){ ?>
+        <a href="perfil.php">Perfil</a>
+        <a href="incio.php?cerrarSesion=true" data-id=<?$_SESSION['ID']?> id='perfil'> Cerrar sesion</a>
+      <?php } elseif($uri != '/login.php'){?>
+        <a href="login.php">login</a>
+      <?php }?>
+        <a href="paginaajax.php">AJAX</a>
     </div>
   </nav>
 </header>

b15d7e980aaa40ff4a8785a1a8816c00d94d7518
Steven-Cadena <steven@gmail.com>
2020-05-01 18:28:44 +0200

    arreglos administrador delete

---
+++ resources/templates/contenido/agregar.php
@@ -0,0 +1,20 @@
+<?php
+
+$nombreHashtag = "";
+
+if (isset($_POST) && count($_POST)!=0 ) {
+  if (isset($_POST['hashtag']) && strlen($_POST['hashtag'])>=1 ) {
+    $nombreHashtag = clear_input($_POST['hashtag']);
+    HashtagManager::insert($nombreHashtag,$_SESSION['ID']);
+  }
+  header('Location: administrador.php');
+  die();
+}
+?>
+
+<h1>Formulario para ingresar un nuevo hashtag</h1>
+<form class="" action="agregar.php" method="post">
+    <label> Ingrese el nuevo Hashtag</label>
+  	<input type="text" name="hashtag" placeholder="Escriba aquí el nuevo hashtag" value="<?=$hashtag?>"><br>
+    <input type="submit" name="agregar" value="agregar"><br>
+</form>
---
+++ resources/templates/contenido/eliminar.php
@@ -0,0 +1,15 @@
+<?php
+
+/*print_r($_GET['ID']);*/
+
+if (isset($_GET) && count($_GET)!=0 ) {
+  if (isset($_GET['hashtag'])) {
+    $id = $_GET['ID'];
+    HashtagManager::delete($id);
+  }
+  header('Location: administrador.php');
+  die();
+}
+
+
+?>

f30ca189a189f9f443399538108c303876831e03
adriansanz94 <adriansanzperez.94@gmail.com>
2020-05-04 18:06:32 +0200

    todo bien todo correcto

--- resources/templates/contenido/listadoHashtag.php
+++ resources/templates/contenido/listadoHashtag.php
@@ -5,27 +5,24 @@
 if(isset($_GET['id'])){
 
   $id = $_GET['id'];
-  as_debug($_GET);
+  //as_debug($_GET);
   $datos = PublicacionManager::getPublicacionByHashtagId($id);
 
 } else if(isset($_GET['id_hashtag'])) {
   //esta obtiene publicaciones por id del hastag
   $id = $_GET['id_hashtag'];
-  as_debug($_GET);
-<<<<<<< HEAD
-  $datos = PublicacionManager::getByHashtagId($id);
+  //as_debug($_GET);
 
-=======
   $datos = PublicacionManager::getPublicacionByHashtagId($id);
   
->>>>>>> 41d3afac3121dea800ed1d689b710077428697e6
+
 }else{
   //esta obtiene todos los hastag y sus publcaciones
   $datos = PublicacionManager::getAllPublicacionByHashtag();
 }
 
 
-as_debug($datos);
+//as_debug($datos);
 
 
 ?>

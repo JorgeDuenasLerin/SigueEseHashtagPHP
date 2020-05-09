6e97a6ae35605497ed78cb4efd260ac4b69ce7d3
adriansanz94 <adriansanzperez.94@gmail.com>
2020-05-09 13:22:49 +0200

    recuperar contraseña falla

--- resources/templates/contenido/recuperarPass.php
+++ resources/templates/contenido/recuperarPass.php
@@ -1,7 +1,9 @@
 <?php 
   //REQUIRES
+  require 'vendor/autoload.php';
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\SMTP;
   require 'vendor/phpmailer/phpmailer/src/Exception.php';
   require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
   require 'vendor/phpmailer/phpmailer/src/SMTP.php';

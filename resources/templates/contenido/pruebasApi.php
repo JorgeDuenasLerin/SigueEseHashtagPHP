b59593c3b0fbb2fb2c9cf000e2b05c2f7a00f203
adriansanz94 <adriansanzperez.94@gmail.com>
2020-05-16 11:36:54 +0200

    primeras pruebas de mostrar datos

--- resources/templates/contenido/pruebasApi.php
+++ resources/templates/contenido/pruebasApi.php
@@ -24,11 +24,27 @@ $data = $twitter->setGetfield($getfield)
     ->performRequest();
 
 $phpData = json_decode($data);
-/*as_debug($phpData);*/
+//as_debug($phpData->statuses[0]);
 
 print_r(count($phpData->statuses));
 
 $o = $phpData->statuses[0];
 /*print_r($phpData->statuses[0]);*/
+as_debug($o);
 
 ?>
+
+<link rel="stylesheet" href="/css/general.css">
+<h1 class="centrar">mostrar peticion twitter</h1><br><br>
+<div class="tabla">
+    <?php foreach($o as $fila) { ?>
+        <p><?=$fila['created_at']?></p>
+        <p><?=$fila['id']?></p>
+        <p><?=$fila['full_text']?></p>
+        
+
+
+    <?php } ?>
+		
+</div>
+

e7384dfeb4963d3111472f3e748c2e6160f7e28b
Steven-Cadena <steven@gmail.com>
2020-05-16 12:27:41 +0200

    arreglos de la api

--- resources/templates/contenido/pruebasApi.php
+++ resources/templates/contenido/pruebasApi.php
@@ -1,59 +1,21 @@
 <?php
 
-require('vendor/autoload.php');
-
-require("config/configuracion.php");
-
-/*print_r($config);*/
-
-$usuario="telepizza";
-$hastag="#pizza";
-
-$turl = 'https://api.twitter.com';
-$petciónURL = "/1.1/search/tweets.json";
-$getfield = "?q=from:$hastag&tweet_mode=extended";
-$url = "$turl$petciónURL";
-$requestMethod = 'GET';
-
-/*echo "$url";*/
 
 
-$twitter = new TwitterAPIExchange($config);
-$data = $twitter->setGetfield($getfield)
-    ->buildOauth($url, $requestMethod)
-    ->performRequest();
+$hashtag = "#coronavirus";
+$obj = peticionApi($hashtag);
 
-$phpData = json_decode($data);
-//as_debug($phpData->statuses[0]);
-
-print_r(count($phpData->statuses));
-
-$o = $phpData->statuses[0];
-/*print_r($phpData->statuses[0]);*/
-as_debug($o);
-/*print_r("id del usuario");
-as_debug($o[0]->id);*/
 
 
 ?>
 
 <link rel="stylesheet" href="/css/general.css">
-<h1 class="centrar">mostrar peticion twitter</h1><br><br>
+<h1 class="centrar">Mostrar peticion twitter</h1><br><br>
 <div class="tabla">
-    <?php foreach($o as $fila) { ?>
-<<<<<<< HEAD
+    <?php foreach($obj as $fila) { ?>
         <p><?=$fila->{'created_at'}?></p>
         <p><?=$fila->{'id'}?></p>
         <p><?=$fila->{'full_text'}?></p>
-=======
-    <?php  print_r($fila)?>
-        <p><?=$fila["created_at"]?></p>
-        <p><?=$fila['id']?></p>
-        <p><?=$fila['full_text']?></p>
-        
-
-
->>>>>>> 3118ff06ef1e24f7d0f1c7ac410feb5e06116e37
     <?php } ?>
 
 </div>

bd3b1d6e3c4e7d216c4ef7f34d610953e2b8ae22
Steven-Cadena <steven@gmail.com>
2020-05-16 12:02:35 +0200

    pruebas api

--- resources/templates/contenido/pruebasApi.php
+++ resources/templates/contenido/pruebasApi.php
@@ -31,6 +31,8 @@ print_r(count($phpData->statuses));
 $o = $phpData->statuses[0];
 /*print_r($phpData->statuses[0]);*/
 as_debug($o);
+/*print_r("id del usuario");
+as_debug($o[0]->id);*/
 
 ?>
 
@@ -38,13 +40,9 @@ as_debug($o);
 <h1 class="centrar">mostrar peticion twitter</h1><br><br>
 <div class="tabla">
     <?php foreach($o as $fila) { ?>
-        <p><?=$fila['created_at']?></p>
-        <p><?=$fila['id']?></p>
-        <p><?=$fila['full_text']?></p>
-        
-
-
+        <p><?=$fila->{'created_at'}?></p>
+        <p><?=$fila->{'id'}?></p>
+        <p><?=$fila->{'full_text'}?></p>
     <?php } ?>
-		
-</div>
 
+</div>

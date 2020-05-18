8fcb966e6604a7f1d664b2f0b66d409dc242f18a
Steven-Cadena <steven@gmail.com>
2020-05-18 11:24:19 +0200

    jugando con la api

--- resources/templates/contenido/pruebasApi.php
+++ resources/templates/contenido/pruebasApi.php
@@ -1,17 +1,33 @@
 <?php
 
-
-
 $hashtag = "#coronavirus";
 $obj = peticionApi($hashtag);
 
+$todoslosHashtag = HashtagManager::getAll();
+$insert;
+
+foreach ($todoslosHashtag as $fila) {
+        $resultado = peticionApi($fila['NOMBRE']);
+        $insertFecha = $resultado['0']->created_at;
+        $insertContenido = $resultado['0']->full_text;
+      /*  PublicacionManager::insert();*/
+        as_debug($insertFecha,"ejemlo de dato");
+        as_debug($resultado);
+}
 
 
 ?>
-
 <link rel="stylesheet" href="/css/general.css">
+
 <h1 class="centrar">Mostrar peticion twitter</h1><br><br>
 <div class="tabla">
+    <?php foreach($resultado as $fila) { ?>
+        <p><?=$fila->{'created_at'}?></p>
+        <p><?=$fila->{'id'}?></p>
+        <p><?=$fila->{'full_text'}?></p>
+    <?php } ?>
+
+    <h1>otro for </h1>
     <?php foreach($obj as $fila) { ?>
         <p><?=$fila->{'created_at'}?></p>
         <p><?=$fila->{'id'}?></p>

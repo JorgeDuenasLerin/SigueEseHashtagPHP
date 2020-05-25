97c52d2ff2c2858e297e67809beaa10dd1a253c9
Steven-Cadena <steven@gmail.com>
2020-05-25 19:42:16 +0200

    arreglo

--- src/funcionesApiTwitter.php
+++ src/funcionesApiTwitter.php
@@ -134,20 +134,6 @@ function insercionEnBBDD(){
                $imagen = $tweet->{'retweeted_status'}->{'extended_entities'}->{'media'}[0]->{'media_url'};
 
                //as_debug($imagen,"imagen antes del if");
-<<<<<<< HEAD
-               as_debug(idTwetExists($idExterno),"existe el id del twet ");
-               if(idTwetExists($idExterno) != 0 || count($countPublicaciones)==0){
-
-                if($imagen != ''){
-                  $urlImagen = guardarImagen($fila['NOMBRE'],$idExterno,$imagen);
-                }else{
-                  $urlImagen= " no contiene imagen";
-                }
-                PublicacionManager::insert($convertidoUsuario,$convertidoContenido,$urlImagen,$fecha,$twitter,$idExterno);
-                $idPublicacion = PublicacionManager::getIdByIdTweet($idExterno);
-                $idHashtag = HashtagManager::getIdByNombre($fila['NOMBRE']);
-                HashpubManager::insert($idHashtag,$idPublicacion);
-=======
                //as_debug(idTwetExists($idExterno),"existe el id del twet ");
                if(idTwetExists($idExterno) /*|| count($countPublicaciones)== 0*/ ){
                 print_r('entro')  ;
@@ -160,7 +146,7 @@ function insercionEnBBDD(){
                   $idPublicacion = PublicacionManager::getIdByIdTweet($idExterno);
                   $idHashtag = HashtagManager::getIdByNombre($fila['NOMBRE']);
                   HashpubManager::insert($idHashtag,$idPublicacion);
->>>>>>> 6b196da9fcf9c40eaf5adfd66666ae1595391bc3
+
                }
               /* as_debug($usuario,"usuario");
                as_debug($fecha,"fecha");
@@ -173,13 +159,8 @@ function insercionEnBBDD(){
 
 function idTwetExists($idExterno){
   $idBd = PublicacionManager::getAllIdTweet($idExterno);
-<<<<<<< HEAD
-  as_debug($idBd,"id de la funcion");
-  if(!$idBd){
-    return true;
-  }else {
-    return false;
-=======
+
+
   //as_debug($idBd,"id de la funcion ");
   $existe = '';
   if($idBd != $idExterno){
@@ -190,7 +171,7 @@ function idTwetExists($idExterno){
     //as_debug($idBd,"si existe");
     //as_debug(false);
     return $existe = false;
->>>>>>> 6b196da9fcf9c40eaf5adfd66666ae1595391bc3
+
   }
 }
 function startsWith ($string, $startString) {
